fb02e6139167e1bb5133a2cf1fecfdf263cae2e0
Artem <example@gmail.com>
2021-07-08 14:15:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { createContext, useEffect, useState } from 'react';
+import Content from './components/Content';
+import Sidebar from './components/Sidebar';
+import PatientService from './components/services/PatientDB';
+
+export const PatientContext = createContext([{}]);
 
 function App() {
+  const [patients, setPatients] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [sidebarPatients, setSidebarPatients] = useState([]);
+  const [selectedPatient, setSelectedPatient] = useState([]);
+
+  async function patientsHasChanged() {
+    const patientService = new PatientService();
+    const loadedPatients = await patientService.getPatients();
+    let newSelectedPatientComments = [];
+    let selectedUserComment = [];
+
+    newSelectedPatientComments.forEach(c => {
+      selectedUserComment = [...selectedUserComment, { id: c.id, comment: c.comment, date: c.date }];
+    });
+
+    setPatients(loadedPatients);
+    setSidebarPatients(loadedPatients);
+
+    setComments(selectedUserComment);
+  }
+
+  useEffect(() => {
+    patientsHasChanged();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <PatientContext.Provider value={{
+        selectedPatient, setSelectedPatient, patients, setPatients, patientsHasChanged, comments, setComments, sidebarPatients, setSidebarPatients }}>
+        <Sidebar />
+        <Content />
+      </PatientContext.Provider>
     </div>
   );
 }
